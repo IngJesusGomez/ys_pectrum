59caa875eb2f205f852f9bfbabd1268f8819ac5e
jesus.gomez <jesus.gomez@gruposynergo.com>
2024-03-08 05:35:32 -0600

    refactor: se empieza login parte 1

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
