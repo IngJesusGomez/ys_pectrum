b21c08d6cf216405ff52174d7d2651e702e94d88
jesus.gomez <jesus.gomez@gruposynergo.com>
2024-03-19 16:13:15 -0600

    feat: se termina vista de login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,14 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule,MatGridListModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  constructor(){
+    
+  }
+
+  registerButton(){
+    console.log("Entra a registrar")
+    document.getElementById("container")?.classList.add("right-panel-active")
+  }
+
+  loginButton(){
+    document.getElementById("container")?.classList.remove("right-panel-active")
+  }
 
 }
